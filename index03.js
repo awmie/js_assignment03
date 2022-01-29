343a0d7a4af80a0a899f068c9f892b4d80a2bf64
awmie <amritavakole@gmail.com>
2022-01-29 17:41:32 +0530

    first commit

---
+++ index03.js
@@ -0,0 +1,8 @@
+const input = prompt("Which multiplication table you want to know?")
+
+function multiplication(){
+    for (i = 0; i <=10; i++){
+        console.log(input*i)
+    }
+}
+multiplication();
